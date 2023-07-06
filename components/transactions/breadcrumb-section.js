a5f0485d802d5593f800b213cb994e9c62d262fc
Furkan Nabi Sumji <103491179+furkannabisumji@users.noreply.github.com>
2023-07-06 12:36:40 +0530

    added transactions history for admin

--- components/transactions/breadcrumb-section.js
+++ components/transactions/breadcrumb-section.js
@@ -5,18 +5,28 @@ export default function BreadcrumbSection(props) {
 		<div className="fugu--breadcrumbs-section">
 			<div className="fugu--breadcrumbs-data center-content">
 				<h1 className="wow fadeInUpX" data-wow-delay="0s">
-					Paid Users
+				Transactions
 				</h1>
 				<Table striped bordered hover variant="dark">
       <thead>
         <tr>
-		  <th>Email</th>
+		  <th>Order Id</th>
+		  <th>Transaction ID</th>
+		  <th>Timestamp</th>
+		  <th>News</th>
+		  <th>Amount</th>
+		  <th>Status</th>
         </tr>
       </thead>
       <tbody>
-		{props.users.map((user,i)=>(
+		{props.transactions.map((transaction,i)=>(
 			    <tr key={i}>
-				<td>{user.userEmail}</td>
+				<td>{transaction.orderID}</td>
+				<td>{transaction.transcationID}</td>
+				<td>{transaction.orderTimeDate}</td>
+				<td>{transaction.newsTitle}</td>
+				<td>{transaction.orderValue}</td>
+				<td>{transaction.genre}</td>
 			  </tr>))}
       </tbody>
     </Table>
