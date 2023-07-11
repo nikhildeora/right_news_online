2c74f3231f2181c457e158deb071a83bf95602ad
aadilwarsi05 <aadilwarsi75@gmail.com>
2023-07-11 20:00:52 +0530

    Paid, Transcaction and other Pages Updated - AW

--- components/users/breadcrumb-section.js
+++ components/users/breadcrumb-section.js
@@ -1,31 +1,54 @@
 import React from "react";
 import { format } from "date-fns";
-import Table from 'react-bootstrap/Table';
+import Table from "react-bootstrap/Table";
 export default function BreadcrumbSection(props) {
-	return (
-		<div className="fugu--breadcrumbs-section">
-			<div className="fugu--breadcrumbs-data center-content">
-				<h1 className="wow fadeInUpX" data-wow-delay="0s">
-					Paid Users
-				</h1>
-				<Table striped bordered hover variant="dark">
-      <thead>
-        <tr>
-		  <th>Email</th>
-		  <th>Active Plan</th>
-		  <td>Plan Expiry</td>
-        </tr>
-      </thead>
-      <tbody>
-		{props.users.map((user,i)=>(
-			    <tr key={i}>
-				<td>{user.userDetails.userEmail}</td>
-				<td>{user.planDetail.planTitle}</td>
-				<td>{format(new Date(user.endtDate), "dd MMMM yyyy")}</td>
-			  </tr>))}
-      </tbody>
-    </Table>
-			</div>
-		</div>
-	);
+  return (
+    <div className="fugu--breadcrumbs-section">
+      <h2
+        className="wow fadeInUpX "
+        data-wow-delay="0s"
+        style={{
+          fontSize: "52px",
+          color: "#fff",
+          textAlign: "center",
+          marginBlock: "-60px 60px",
+          letterSpacing: "1.5px",
+        }}
+      >
+        PAID USERS
+      </h2>
+      <div className="fugu--breadcrumbs-data center-content">
+        <Table
+          striped
+          bordered
+          hover
+          variant="dark"
+          style={{ border: "2px solid #fff", textAlign: "center" }}
+        >
+          <thead>
+            <tr>
+              <th style={{ paddingBlock: "18px 18px" }}>Email</th>
+              <th style={{ paddingBlock: "18px 18px" }}>Active Plan</th>
+              <td style={{ paddingBlock: "18px 18px" }}>Plan Expiry</td>
+            </tr>
+          </thead>
+          <tbody>
+            {props.users.map((user, i) => (
+              <tr key={i}>
+                <td style={{ paddingBlock: "15px 15px" }}>
+                  {user.userDetails.userEmail}
+                </td>
+                <td style={{ paddingBlock: "15px 15px" }}>
+                  {user.planDetail.planTitle}
+                </td>
+                <td style={{ paddingBlock: "15px 15px" }}>
+                  {format(new Date(user.endtDate), "dd MMMM yyyy")}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </div>
+    </div>
+  );
 }
