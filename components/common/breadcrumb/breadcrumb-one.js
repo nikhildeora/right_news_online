6cec10c88e698104ae97cdc2545f4001aa3aff9f
Rahul Mane <ramborau@gmail.com>
2023-07-12 01:52:37 +0530

    New

--- components/common/breadcrumb/breadcrumb-one.js
+++ components/common/breadcrumb/breadcrumb-one.js
@@ -1,30 +1,21 @@
-import Link from "next/link";
+import Link from 'next/link';
 
 /* eslint-disable @next/next/no-img-element */
 export default function BreadcrumbOne({ title }) {
-  return (
-    <div className="fugu-breadcrumb-section">
-      <div className="container">
-        <div className="breadcrumbs">
-          <h1>{title}</h1>
-          <nav aria-label="breadcrumb">
-            <ol className="breadcrumb">
-              <li className="breadcrumb-item" style={{ fontWeight: "600" }}>
-                <Link href={"/"}>Home</Link>
-              </li>
-              <li className="breadcrumb-item active" aria-current="page">
-                {title}
-              </li>
-            </ol>
-          </nav>
-        </div>
-      </div>
-      <div className="fugu-shape8">
-        <img src="/images/shape/shape7.png" alt="" />
-      </div>
-      <div className="fugu-shape9">
-        <img src="/images/shape/shape8.png" alt="" />
-      </div>
-    </div>
-  );
+	return (
+		<div className="fugu-breadcrumb-section">
+			<div className="container">
+				<div className="breadcrumbs text-white">
+					<h1 className="text-white">{title}</h1>
+					<h2>Strategy pricing plans that we offer to our clients</h2>
+				</div>
+			</div>
+			<div className="fugu-shape8">
+				<img src="/images/shape/shape7.png" alt="" />
+			</div>
+			<div className="fugu-shape9">
+				<img src="/images/shape/shape8.png" alt="" />
+			</div>
+		</div>
+	);
 }
